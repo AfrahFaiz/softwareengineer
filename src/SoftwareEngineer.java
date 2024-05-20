f0b19b63356713822289f33b22f530c92a001b60
afrah faiz <fathimaafrah2022@gmail.com>
2024-05-20 15:36:44 +0530

    hi

---
+++ src/SoftwareEngineer.java
@@ -0,0 +1,30 @@
+public class SoftwareEngineer {
+
+    //attributes
+
+    String name;
+    String programming_language;
+    String favourite_coding;
+
+
+
+    SoftwareEngineer (String name,String programming_language,String favourite_coding){
+
+           this. name= name;
+           this.programming_language=programming_language;
+           this.favourite_coding=favourite_coding;
+
+
+
+}
+
+//method
+
+    //study
+
+//
+
+   void study() {
+       System.out.println("SoftwareEngineer can study");
+   }
+}
---
+++ src/main1.java
@@ -0,0 +1,17 @@
+public class main1 {
+
+    public static void main(String[] args) {
+
+
+        SoftwareEngineer obj1 = new SoftwareEngineer("John","english","java");
+
+
+        System.out.println(obj1.name);
+
+
+        obj1.study();
+
+
+
+    }
+}
